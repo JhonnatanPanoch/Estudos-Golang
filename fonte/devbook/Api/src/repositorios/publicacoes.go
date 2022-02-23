87117d03bebb0ae7c6e1bac97b031bc4f27478c0
JhonnatanPanoch <43388041+JhonnatanPanoch@users.noreply.github.com>
2022-02-23 16:53:38 -0300

    Rota de atualizar publicacao

--- fonte/devbook/Api/src/repositorios/publicacoes.go
+++ fonte/devbook/Api/src/repositorios/publicacoes.go
@@ -100,3 +100,18 @@ func (p Publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, error) {
 
 	return publicacoes, nil
 }
+
+// Atualizar altera os dados de uma publicação no banco de dados
+func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao modelos.Publicacao) error {
+	statement, erro := repositorio.db.Prepare("update publicacoes set titulo = ?, conteudo = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
