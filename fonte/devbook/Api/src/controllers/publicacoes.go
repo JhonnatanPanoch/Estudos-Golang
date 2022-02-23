2497b1e4ed69b5ad282361c04b937c7e29870f38
JhonnatanPanoch <43388041+JhonnatanPanoch@users.noreply.github.com>
2022-02-23 16:59:03 -0300

    Rota para excluir uma publicação

--- fonte/devbook/Api/src/controllers/publicacoes.go
+++ fonte/devbook/Api/src/controllers/publicacoes.go
@@ -112,23 +112,23 @@ func BuscarPublicacao(rw http.ResponseWriter, r *http.Request) {
 }
 
 // AtualizarPublicacao efetua a atualização de uma publicação
-func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+func AtualizarPublicacao(rw http.ResponseWriter, r *http.Request) {
 	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
 	if erro != nil {
-		respostas.Erro(w, http.StatusUnauthorized, erro)
+		respostas.Erro(rw, http.StatusUnauthorized, erro)
 		return
 	}
 
 	parametros := mux.Vars(r)
 	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
 	if erro != nil {
-		respostas.Erro(w, http.StatusBadRequest, erro)
+		respostas.Erro(rw, http.StatusBadRequest, erro)
 		return
 	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
-		respostas.Erro(w, http.StatusInternalServerError, erro)
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
 		return
 	}
 	defer db.Close()
@@ -136,41 +136,78 @@ func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
 	repositorio := repositorios.CriarRepositorioPublicacoes(db)
 	publicacaoSalvaNoBanco, erro := repositorio.BuscarPorId(publicacaoID)
 	if erro != nil {
-		respostas.Erro(w, http.StatusInternalServerError, erro)
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
 		return
 	}
 
 	if publicacaoSalvaNoBanco.AutorID != usuarioID {
-		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possível atualizar uma publicação que não seja sua"))
+		respostas.Erro(rw, http.StatusForbidden, errors.New("não é possível atualizar uma publicação que não seja sua"))
 		return
 	}
 
 	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
-		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		respostas.Erro(rw, http.StatusUnprocessableEntity, erro)
 		return
 	}
 
 	var publicacao modelos.Publicacao
 	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
-		respostas.Erro(w, http.StatusBadRequest, erro)
+		respostas.Erro(rw, http.StatusBadRequest, erro)
 		return
 	}
 
 	if erro = publicacao.Preparar(); erro != nil {
-		respostas.Erro(w, http.StatusBadRequest, erro)
+		respostas.Erro(rw, http.StatusBadRequest, erro)
 		return
 	}
 
 	if erro = repositorio.Atualizar(publicacaoID, publicacao); erro != nil {
-		respostas.Erro(w, http.StatusInternalServerError, erro)
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
 		return
 	}
 
-	respostas.JSON(w, http.StatusNoContent, nil)
+	respostas.JSON(rw, http.StatusNoContent, nil)
 }
 
 // ApagarPublicacao apaga uma publicação do usuário logado
 func ApagarPublicacao(rw http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(rw, http.StatusUnauthorized, erro)
+		return
+	}
 
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(rw, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.CriarRepositorioPublicacoes(db)
+	publicacaoSalvaNoBanco, erro := repositorio.BuscarPorId(publicacaoID)
+	if erro != nil {
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if publicacaoSalvaNoBanco.AutorID != usuarioID {
+		respostas.Erro(rw, http.StatusForbidden, errors.New("não é possível deletar uma publicação que não seja sua"))
+		return
+	}
+
+	if erro = repositorio.Deletar(publicacaoID); erro != nil {
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(rw, http.StatusNoContent, nil)
 }
