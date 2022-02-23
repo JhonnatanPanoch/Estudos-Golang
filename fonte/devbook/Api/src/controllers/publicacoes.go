82ca1fa1d1ee33afb2da4604e51c9cdef412fa2a
JhonnatanPanoch <jhonnatan.jp@gmail.com>
2022-02-23 14:54:24 -0300

    Controller e repositorio de publicações
    Metodo de incluir publicacao no banco

--- fonte/devbook/Api/src/controllers/publicacoes.go
+++ fonte/devbook/Api/src/controllers/publicacoes.go
@@ -1,10 +1,53 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 // CriarPublicao efetua a criação de uma nova publicação
 func CriarPublicao(rw http.ResponseWriter, r *http.Request) {
+	usuarioId, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(rw, http.StatusBadRequest, erro)
+		return
+	}
 
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(rw, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		respostas.Erro(rw, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = usuarioId
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
+		return
+	}
+	db.Close()
+
+	repositorio := repositorios.CriarRepositorioPublicacoes(db)
+	idPublicacao, erro := repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(rw, http.StatusCreated, idPublicacao)
 }
 
 // BuscarPublicacacoes traz as publicações que aparecem no feed do usuário
---
+++ fonte/devbook/Api/src/repositorios/publicacoes.go
@@ -0,0 +1,40 @@
+package repositorios
+
+import (
+	"api/src/modelos"
+	"database/sql"
+)
+
+// Publicacoes representa um repositorio de publicações
+type Publicacoes struct {
+	db *sql.DB
+}
+
+func CriarRepositorioPublicacoes(db *sql.DB) *Publicacoes {
+	return &Publicacoes{db}
+}
+
+func (p Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, error) {
+
+	var query = `INSERT INTO publicacoes (titulo, conteudo, autor_id) VALUES (?, ?, ?)`
+	statement, erro := p.db.Prepare(query)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	insercao, erro := statement.Exec(
+		publicacao.Titulo,
+		publicacao.Conteudo,
+		publicacao.AutorID)
+	if erro != nil {
+		return 0, erro
+	}
+
+	idInserido, erro := insercao.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(idInserido), nil
+}
